2fe1392454eca30ab1aca1b0f860d11d44cc1882
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-01-17 07:46:32 +0100

    chore(form-view):
    add bootstrapp to views

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
-
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings.base_settings")
+application = Cling(get_wsgi_application())
