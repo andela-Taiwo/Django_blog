6011a90f8ff7a3a8fea1716b4f5cf1e6fe76b44f
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-01-19 09:02:14 +0100

    add logic for test environment in settings  add a post detail page and route  add post update page

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
-
 from posts.models import Post, User
 from .forms import SignUpForm, ContactForm, SignInForm, PostForm
 
@@ -13,6 +13,7 @@ from .forms import SignUpForm, ContactForm, SignInForm, PostForm
 class MyView(View):
     template_name = 'posts.html'
     STATIC_URL = settings.STATIC_URL
+    http_method_names = ['get', 'POST', 'put', 'patch', 'delete']
 
     def get(self, request, *args, **kwargs):
         # <view logic>
@@ -30,18 +31,13 @@ class MyView(View):
     def post(self, request, *args, **kwargs):
         title = "Blog suceessfully posted"
         form = PostForm(request.POST or None)
-        # import pdb; pdb.set_trace
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.user_id = User.objects.get(pk=request.user.pk)
-            post.save()
-
-        context = {
-            'title': title,
-            'STATIC_URL': settings.STATIC_URL
-        }
-        return render(request, 'posts.html', context)
+        if request.method == 'POST':
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.user_id = User.objects.get(pk=request.user.pk)
+                post.save()
+            return HttpResponseRedirect('/posts')
 
 
 def home(request):
@@ -85,6 +81,35 @@ def register(request):
     return render(request, 'index.html', context)
 
 
+def post_update(request, id):
+    instance = get_object_or_404(Post, pk=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        # messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
+        return HttpResponseRedirect('/posts')
+
+    context = {
+		"title": instance.title,
+		"instance": instance,
+		"form":form,
+	}
+    return render(request, "post_edit.html", context)
+
+
+def post_detail(request, id):
+    instance = get_object_or_404(Post, pk=id)
+    if instance:
+        context = {
+            "title": instance.title,
+            "post": instance,
+            "STATIC_URL": settings.STATIC_URL
+            }
+        return render(request, "post_detail.html", context)
+    return HttpResponseRedirect('/posts')
+
+
 def contact(request):
     form = ContactForm(request.POST or None)
     context = {
@@ -102,7 +127,7 @@ def contact(request):
             send_mail('Subject here', 'Here is the message.',
                       'sokunbimaimunah@gmail.com',
                       ['sokunbimaimunah@gmail.com'], fail_silently=False)
-            print("yay workd")
+            print("yay worked")
         except ValueError:
             raise ValueError
 
