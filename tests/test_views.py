906e3ed5fd9a79074cbfce7c921d6448207a20d7
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-01-19 09:02:14 +0100

    add logic for test environment in settings

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-from posts.models import User
-from django.utils import timezone
+from django.core.urlresolvers import resolve
+
+from django.test import TestCase
+from posts.views import home
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
