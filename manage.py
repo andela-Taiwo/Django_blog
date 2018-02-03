e31b3ed8413b3c0ce8648589a318ddfa2b61fe8c
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-02-03 16:48:54 +0100

    add settings for prodcution

--- manage.py
+++ manage.py
@@ -8,6 +8,6 @@ import dotenv
 if __name__ == "__main__":
     dotenv.read_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "blog.settings.base_settings")
+                          "blog.settings.prod_settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
